ffa7ab14239ab20eb084b43c2e581d171e62f743
Spencer Knight <SpencerKnight2019@gmail.com>
2025-04-30 12:52:57 -0600

    Wednesday class commiting.

---
+++ week2/discoutn.py
@@ -0,0 +1,22 @@
+#import datetime 
+
+#current_time = datetime.datetime.now()
+
+#formated_time = current_time.strftime("%m/%d/%Y %I:%M %p")
+
+#print(f'{formated_time}')
+
+#now = datetime.datetime.now()
+
+#weekday_number = now.weekday()
+
+#print(f"{weekday_number}")
+
+import date_time_weekday
+
+#print(date_time_weekday.weekday_number)
+
+#print(date_time_weekday.formated_time)
+
+subtotal
+
