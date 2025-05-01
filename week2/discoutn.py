b0e23b844236ae0f98cc48af5cc80fa7ba67ebfd
Spencer Knight <SpencerKnight2019@gmail.com>
2025-05-01 16:24:24 -0600

    saving

--- week2/discoutn.py
+++ week2/discoutn.py
@@ -1,22 +1,16 @@
 #import datetime 
-
 #current_time = datetime.datetime.now()
-
 #formated_time = current_time.strftime("%m/%d/%Y %I:%M %p")
-
-#print(f'{formated_time}')
-
+##print(f'{formated_time}')
 #now = datetime.datetime.now()
-
 #weekday_number = now.weekday()
+##print(f"{weekday_number}")
 
-#print(f"{weekday_number}")
+import date_time_weekday #importing code that is listed above.
 
-import date_time_weekday
+print(date_time_weekday.weekday_number)
 
-#print(date_time_weekday.weekday_number)
+print(date_time_weekday.formated_time)
 
-#print(date_time_weekday.formated_time)
-
-subtotal
+subtotal = 
 
